bd5f58889ec4b52422268ddb6ac12305393c244c
Duy Viec <duyviec@outlook.com>
2017-11-05 21:34:37 +0700

    Add sample rest api

---
+++ src/main/java/com/ecash/cmsapi/api/BaseApi.java
@@ -0,0 +1,10 @@
+package com.ecash.cmsapi.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "${api.url.rootPath}")
+public class BaseApi {
+
+}
