88f32aa28a3db7c1b75b37ebf4559355e854ca67
tcvuongsofm <congvuong1502@gmail.com>
2017-11-06 22:17:56 +0700

    EC-7 Input card information

--- src/main/java/com/ecash/cmsapi/api/BaseApi.java
+++ src/main/java/com/ecash/cmsapi/api/BaseApi.java
@@ -1,10 +1,17 @@
 package com.ecash.cmsapi.api;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "${api.url.rootPath}")
 public class BaseApi {
-
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<?> badRequestExceptionHandler(Exception ex) {
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex);
+  }
 }
+
