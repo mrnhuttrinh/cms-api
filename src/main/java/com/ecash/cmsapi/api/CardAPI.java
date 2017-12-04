7ac7e66275e7e1d34898347de2bdac575c15531f
tcvuongsofm <congvuong1502@gmail.com>
2017-12-04 22:30:58 +0700

    Add search Card list

--- src/main/java/com/ecash/cmsapi/api/CardAPI.java
+++ src/main/java/com/ecash/cmsapi/api/CardAPI.java
@@ -3,8 +3,12 @@ package com.ecash.cmsapi.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.querydsl.binding.QuerydslPredicate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,9 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ecash.cmsapi.api.constant.ResponseConstant;
 import com.ecash.cmsapi.vo.ResponseBodyVO;
 import com.ecash.ecashcore.model.Card;
+import com.ecash.ecashcore.model.Customer;
 import com.ecash.ecashcore.service.CardService;
 import com.ecash.ecashcore.vo.InputCardVO;
 import com.ecash.ecashcore.vo.request.UpdateCardStatusRequestVO;
+import com.querydsl.core.types.Predicate;
 
 @RestController
 public class CardAPI extends BaseApi {
@@ -33,5 +39,12 @@ public class CardAPI extends BaseApi {
   @RequestMapping(value = "${api.url.card.updateStatus}", method = RequestMethod.POST, produces = "application/json; charset=UTF-8")
   public ResponseEntity<Card> updateCardStatus(@RequestBody UpdateCardStatusRequestVO request) {
     return ResponseEntity.ok(cardService.updateCardStatus(request));
+  }
+  
+  @GetMapping(value = "/cards/search")
+  @PreAuthorize(value = "hasPermission(null, 'FULL_CONTROL')")
+  public Iterable<Card> searchAll(@QuerydslPredicate(root = Card.class) Predicate predicate,
+      Pageable pageable) {
+    return cardService.findAll(predicate, pageable);
   }
 }
