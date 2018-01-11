89df861317efa4656c6f76fd62c88ff81f28f009
Duy Viec <duyviec@outlook.com>
2018-01-11 13:24:07 +0700

    General Report API

---
+++ src/main/java/com/ecash/cmsapi/api/ReportApi.java
@@ -0,0 +1,26 @@
+package com.ecash.cmsapi.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecash.ecashcore.service.ReportService;
+import com.ecash.ecashcore.vo.ReportVO;
+
+@RestController
+public class ReportApi extends BaseApi
+{
+  @Autowired
+  private ReportService reportService;
+
+  @GetMapping(value = "${api.url.report.general}")
+  public ResponseEntity<?> getGeneralReport(
+      @RequestParam(name = "fromDate", required = true, defaultValue = "") String fromDate,
+      @RequestParam(name = "toDate", required = true, defaultValue = "") String toDate)
+  {
+    ReportVO result = reportService.getReportGeneral(fromDate, toDate);
+    return ResponseEntity.ok(result);
+  }
+}
