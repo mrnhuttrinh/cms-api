8e9f9b81bfc20ca08415a04c1752b7db1aeea033
Trinh Nguyen Nhut <nntrinh@tma.com.vn>
2018-05-13 12:30:44 +0700

    Customer change status

--- src/main/java/com/ecash/cmsapi/api/CustomerApi.java
+++ src/main/java/com/ecash/cmsapi/api/CustomerApi.java
@@ -1,14 +1,21 @@
 package com.ecash.cmsapi.api;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.querydsl.binding.QuerydslPredicate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecash.ecashcore.model.cms.Customer;
 import com.ecash.ecashcore.service.CustomerService;
+import com.ecash.ecashcore.vo.CustomerVO;
 import com.querydsl.core.types.Predicate;
 
 @RestController
@@ -22,5 +29,19 @@ public class CustomerApi extends BaseApi {
   public Iterable<Customer> searchAll(@QuerydslPredicate(root = Customer.class) Predicate predicate,
       Pageable pageable) {
     return customerService.findAll(predicate, pageable);
+  }
+
+  @PreAuthorize(value = "hasPermission(null, 'CUSTOMER_DETAILS/UPDATE')")
+  @RequestMapping(value = "${api.url.customers.lockCustomers}", method = RequestMethod.POST, produces = "application/json; charset=UTF-8")
+  public ResponseEntity<?> lockCustomers(@RequestBody List<Customer> customers) {
+    customerService.lockCustomers(customers);
+    return ResponseEntity.ok(customers);
+  }
+  
+  @PreAuthorize(value = "hasPermission(null, 'CUSTOMER_DETAILS/UPDATE')")
+  @RequestMapping(value = "${api.url.customers.unlockCustomers}", method = RequestMethod.POST, produces = "application/json; charset=UTF-8")
+  public ResponseEntity<?> unlockCustomers(@RequestBody List<Customer> customers) {
+    customerService.unlockCustomers(customers);
+    return ResponseEntity.ok(customers);
   }
 }
